b731fc61f8cbca2280b19abd32051eba3dfe615d
Juliana Martins <julianaalvesmartins1@gmail.com>
2025-11-07 23:58:26 -0300

    commit 2 aula 9

--- projeto_controle/app_treinamento/admin.py
+++ projeto_controle/app_treinamento/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Treinamento, TreinamentoColaborador
 
 # Register your models here.
+@admin.register(Treinamento)
+class TreinamentoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'validade_em_meses', 'quantidade_horas_formatada')
+
+
+
+@admin.register(TreinamentoColaborador)
+class TreinamentoColaboradorAdmin(admin.ModelAdmin):
+    list_display = ('treinamento', 'colaborador', 'data_treinamento', 'dias_restantes_do_treinamento', 'treinamento_esta_em_dia')
+    search_fields = ('treinamento', 'colaborador')  
